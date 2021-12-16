ed1c771e6ef9d49cc13e9da61ae4e603c0dc0a0c
mackyyy16 <maciek_poreba@interia.eu>
2021-12-16 08:27:23 +0100

    Stylowanie podsumowania

--- WarszatWEB/src/app/shop/summary/summary.component.ts
+++ WarszatWEB/src/app/shop/summary/summary.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { SharedParameters } from '../shared/shared-parameters';
 
 @Component({
    templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.css']
 })
 export class SummaryComponent {
+  totalValue: number=SharedParameters.costSummary.globalTotalValue;
+  transportCost: number=SharedParameters.costSummary.globalTransportCost;
+  paymentCost: number=SharedParameters.costSummary.globalPaymentCost;
+  sum: number=SharedParameters.costSummary.globalSum;
 }
